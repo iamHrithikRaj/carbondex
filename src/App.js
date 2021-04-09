7707b5219ed358d286e5e9102f4b645307766cec
Hrithik Raj <iamhrithikraj26@gmail.com>
2021-04-09 09:58:31 +0530

    added UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Creators from "./components/Creators";
+import Footer from "./components/Footer";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact href="/">
+          <Header state={state} setState={setState} />
+          <About />
+          <Creators />
+          <Footer />
+        </Route>
+        <Route exact href="/calculator">
+          <Dashboard state={state} />
+          <Footer />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
