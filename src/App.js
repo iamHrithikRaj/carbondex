1a545f2c28cbae32715c0b4a13e960eaa3e33383
Hrithik Raj <iamhrithikraj26@gmail.com>
2021-04-09 10:49:28 +0530

    changed HashRouter -> BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Creators from "./components/Creators";
 import Footer from "./components/Footer";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 
 const App = () => {
