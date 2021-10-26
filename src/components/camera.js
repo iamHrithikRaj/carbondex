09e70d108feb9e7bad85d116b6e40984cb28782e
Prakul Tiwari <prakultiwari28@gmail.com>
2021-10-27 00:58:47 +0530

    camera added

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,89 +1,13 @@
-import React, { useRef, useState } from 'react';
-import { Measure } from 'react-measure';
-import { useUserMedia } from './useUserMedia ';
-import { useCardRatio } from './useCardRatio';
-import { useOffsets } from './useOffsets';
+import React from 'react';
+import Webcam from "react-webcam";
 
-const CAPTURE_OPTIONS = {
-    audio: false,
-    video: { facingMode: "environment" },
+const videoConstraints = {
+  facingMode: { exact: "environment" }
 };
-
-function Camera() {
-  const videoRef = useRef();
-  const mediaStream = useUserMedia(CAPTURE_OPTIONS);
-  const [container, setContainer] = useState({ height: 0, width: 0 });
-  const [aspectRatio, calculateRatio] = useCardRatio(1.586);
-  const [isCanvasEmpty, setIsCanvasEmpty] = useState(true);
-  const offsets = useOffsets(
-    videoRef.current && videoRef.current.videoWidth,
-    videoRef.current && videoRef.current.videoHeight,
-    container.width,
-    container.height
-  );
-
-  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
-    videoRef.current.srcObject = mediaStream;
-  }
-
-  function handleResize(contentRect) {
-    setContainer({
-      height: Math.round(contentRect.bounds.width / aspectRatio),
-      width: contentRect.bounds.width
-    });
-  }
-
-  function handleCanPlay() {
-    calculateRatio(videoRef.current.videoHeight, videoRef.current.videoWidth);
-    videoRef.current.play();
-  }
-
-  function handleCapture() {
-    const context = canvasRef.current.getContext("2d");
-
-    context.drawImage(
-      videoRef.current,
-      offsets.x,
-      offsets.y,
-      container.width,
-      container.height,
-      0,
-      0,
-      container.width,
-      container.height
-    );
-
-    canvasRef.current.toBlob(blob => onCapture(blob), "image/jpeg", 1);
-    setIsCanvasEmpty(false);
-  }
-
-  function handleClear() {
-    const context = canvasRef.current.getContext("2d");
-    context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    onClear();
-    setIsCanvasEmpty(true);
-  }
-
+export default function camera() {
   return (
-    <Measure bounds onResize={handleResize}>
-      {({ measureRef }) => (
-        <div>
-            <div ref={measureRef} style={{ height: `${container.height}px` }}>
-              <video 
-                ref={videoRef}
-                onCanPlay={handleCanPlay}
-                style={{ top: `-${offsets.y}px`, left: `-${offsets.x}px` }} 
-                autoPlay 
-                playsInline 
-                muted
-              />
-            </div>
-
-          <button onClick={isCanvasEmpty ? handleCapture : handleClear}>
-            {isCanvasEmpty ? "Take a picture" : "Take another picture"}
-          </button>
-        </div>
-      )}
-    </Measure>
-  );
+    <div>
+      <Webcam height={600} videoConstraints={videoConstraints}/>
+    </div>
+  )
 }
--- src/components/play.js
+++ src/components/play.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../styles/landingPage.css'
 import backimg from '../back.png'
+import Camera from './camera'
+import { Link } from 'react-router-dom';
 
 export default function play() {
     return (
         <div className='frame'>
             <img src={backimg} className='back'/>
+            <div className='cameraframe'>
+                <Camera />
+            </div>
         </div>
     )
 }
